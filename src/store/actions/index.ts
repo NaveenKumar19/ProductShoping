0c623743207c1757c6e48da9be320a44fd35c73a
NVB <60278+NVB@users.noreply.github.ford.com>
2023-10-08 13:27:40 +0530

    remove consoles

--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -10,11 +10,9 @@ export const fetchProducts = () => {
       const response = await fetch(
         'https://my-json-server.typicode.com/benirvingplt/products/products',
       );
-      console.log('shsshhs', response);
       const products: Product[] = await response.json();
       dispatch({type: 'FETCH_PRODUCTS_SUCCESS', payload: products});
     } catch (error) {
-      console.log('shsshhs', error);
       dispatch({
         type: 'FETCH_PRODUCTS_FAILURE',
         payload: JSON.stringify(error),
